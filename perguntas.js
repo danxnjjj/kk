cab527edbd2757db24609c033b17d7e86e9a565c
kkkkd <danilosp1267@gmail.com>
2024-11-29 17:11:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quantas Copas do Mundo o Brasil venceu?',
+    'O Brasil venceu 5 Copas do Mundo.'
+);
+
+criaCartao(
+    'Corinthians',
+    'Em que ano o Corinthians foi fundado?',
+    'O Corinthians foi fundado em 1910.'
+);
+
+criaCartao(
+    'Futebol',
+    'O que significa a sigla FIFA?',
+    'FIFA significa Federação Internacional de Futebol Associado.'
+);
+
+criaCartao(
+    'Corinthians',
+    'Qual o apelido da torcida do Corinthians?',
+    'A torcida do Corinthians é conhecida como Fiel.'
+);
